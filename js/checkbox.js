7437de093f6ae2e75cb576176ecf2635af001176
childhoodisend <morozvktv@gmail.com>
2019-12-13 22:25:53 +0300

    succeed

--- js/checkbox.js
+++ js/checkbox.js
@@ -10,17 +10,65 @@ function calculate(){
 
 		}
 	}
+	/*
+	от 3 10%
+	от 5 20%
+	от 10 30%
+	от 15 40%
+	от 24 50%
+	*/
 	var multiplie = 1.0;
 	var discount;
-	if(counter >= 3 && counter <= 5){
+	if(counter >= 3 && counter < 5){
+		multiplie = 0.9;
+	}
+	if(counter >= 5 && counter < 10)
+	{
 		multiplie = 0.8;
 	}
+	if (counter >= 10 && counter < 15) {
+		multiplie = 0.7;
+	}
+	if (counter >= 15 && counter < 24) {
+		multiplie = 0.6;
+	}
+	if (counter >= 24) {
+		multiplie = 0.5;
+	}
+
+	discount = (1.0 - multiplie) * 100.0;//скидка
+	if(counter < 3)
+	document.getElementsByClassName('sumup_without_discount')[0].getElementsByTagName('h2')[0].style.textDecorationLine = "none";
+	else
+	document.getElementsByClassName('sumup_without_discount')[0].getElementsByTagName('h2')[0].style.textDecorationLine = "line-through";
+
+	document.getElementById('discount').textContent = Math.round(discount);
+	document.getElementById('sumup_without_discount').textContent = money;
+	if(multiplie != 1.0){
+
+	document.getElementById('sumup_with_discount').textContent = (money * multiplie) + " Руб";
+	}
+	else
+	document.getElementById('sumup_with_discount').textContent = " ";
 
 
-	discount = (1.0 - multiplie) * 100.0;
-	money = money * multiplie;
-	alert(counter + " " + money);
+
+
+//сколько осталось до очередной скидки
+	if(counter < 3)
+	document.getElementById('recipe_to_discount_').textContent = 3 - counter;
+	else if(counter < 5)
+	document.getElementById('recipe_to_discount_').textContent = 5 - counter;
+	else if(counter < 10)
+	document.getElementById('recipe_to_discount_').textContent = 10 - counter;
+	else if(counter < 15)
+	document.getElementById('recipe_to_discount_').textContent = 15 - counter;
+	else if(counter < 24)
+	document.getElementById('recipe_to_discount_').textContent = 24 - counter;
+	else if(counter >= 24)
+		document.getElementById('recipe_to_discount_').textContent = 0;
 }
+
 
 
 
@@ -28,16 +76,15 @@ function setcolor_and_sumup(val){
 	var chbox = document.getElementById(val);
 	if(chbox.checked){
 		chbox.parentNode.style.background = "#31E08C";
-
-
-		/*console.log(chbox.parentNode)
-		var span = chbox.parentNode.getElementsByTagName("span")[0];
-		var money = parseInt(span.innerHTML);
-		alert(money);
-		*/
+		chbox.parentNode.getElementsByClassName('text_box')[0].getElementsByTagName('h1')[0].style.color = "#FFFFFF";
+		chbox.parentNode.getElementsByClassName('text_box')[0].getElementsByTagName('h2')[0].style.color = "#FFFFFF";
+		chbox.parentNode.getElementsByClassName('price')[0].style.color = "#FFFFFF";
 	}
 	else{
 	chbox.parentNode.style.background = "#FFFFFF";
+	chbox.parentNode.getElementsByClassName('text_box')[0].getElementsByTagName('h1')[0].style.color = "#000000";
+	chbox.parentNode.getElementsByClassName('text_box')[0].getElementsByTagName('h2')[0].style.color = "#000000";
+	chbox.parentNode.getElementsByClassName('price')[0].style.color = "#000000";
 	}
 	calculate();
  }
